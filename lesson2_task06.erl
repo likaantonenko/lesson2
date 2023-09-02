ac7433bec066d6c17ce6b44d05f632dc659743fa
Angelika Antonenko <106235173+likaantonenko@users.noreply.github.com>
2023-09-03 00:00:06 +0300

    add change to task06

--- lesson2_task06.erl
+++ lesson2_task06.erl
@@ -1,5 +1,9 @@
 -module(lesson2_task06).
--export([is_palindrome/1, reverse/1]).
+-export([is_palindrome/1, is_palindrome/2, reverse/1, reverse/2]).
+
+reverse(List)->reverse(List,[]).
+reverse([],Acc)->Acc;
+reverse([H|T],Acc)->reverse(T,[H|Acc]).
 
 is_palindrome(List) -> is_palindrome(List, reverse(List)).
 is_palindrome([], []) -> 
